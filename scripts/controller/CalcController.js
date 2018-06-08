e758ed8d87bd03f3ebcfce55a4f85b07df3ff42e
Marcus Almeida <s030827@gmail.com>
2018-06-08 11:04:55 -0300

    Treat an error on GetResult

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -164,7 +164,17 @@ class CalcController {
   }
 
   getResult(){
-    return eval(this._operation.join(""));
+    
+    try {
+
+      return eval(this._operation.join(""));
+
+    } catch(e){
+
+      setTimeout(()=>{
+        this.setError();
+      }, 1);
+    }
   }
 
   calc() {
